be7b824f45fd14aa51c689d7be5ee25d08325112
Sweta Thakare <143093330+Swetathakare@users.noreply.github.com>
2024-03-29 22:22:12 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,45 @@
-//Alumni Class
+package bufferpackage;
+//Alumini Class
+import java .util.*;
+class Alumni{
+	
+	Post p[];
+	class Post{
+		
+		
+	}
+	HashMap<Integer,Alumni> hm = new HashMap<>();
+	
+	
+	//hashmap = alumni id - details (Node Alumni)
+}
 
+class Student{
+	
+}
+public class Main {
 
-//Student class
+	public static void main(String[] args) {
+		
+//ask if alumni or student 
+		//if student
+		
+		// see posts of alumni
+		   // show posts of all alumni 
+		   // open post based on post id entered by student
+		// search alumni
+		   // see particular alumni
+		   //show profile and details of that alumni 
+		   // posts
+		// search alumni based on category 
+		
+		//if alumni
+		
+		// already logged in or sign up
+		// create account - details 
+		// store in db
+		// login check for password and log in accordingly
+		
+	}
+
+}
