304fc1d3bcb6d9a63c8c75e20ec0f816fde1fefa
SAKSHI <94298487+Sakshi-1602@users.noreply.github.com>
2024-04-11 14:18:38 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -471,6 +471,31 @@ public class Main {
 		return new Student(rollNum, name, contactNo, emailId, branch, yearOfStudy, GPA, areaOfInterest, workExperience);
 
 		}
+	static boolean checkStudent(String username, String password) {
+        // Dummy implementation for checking student credentials
+        return true; // Returning true for demonstration
+    }
+static void seeAlumniByPreference() {
+        // Options based on alumni preferences
+        System.out.println("------------------\n" + "1) Based on branch \n" + "2) Based on passing year \n" + "3) Based on domain \n" + "4) Based on organization\n--------------------");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                //seeAlumniByBranch();
+                break;
+            case 2:
+                //seeAlumniByPassingYear();
+                break;
+            case 3:
+                //seeAlumniByDomain();
+                break;
+            case 4:
+                //seeAlumniByOrganization();
+                break;
+            default:
+                System.out.println("Invalid Choice !");
+                break;
+        }
 
 	public static void main(String[] args) {
 		AlumniDB obj=new AlumniDB();
@@ -526,8 +551,57 @@ public class Main {
 			}else {
 				System.out.println("Invalid Choice !");
 			}
-		}else if(choice ==2) {
-			//Student
+		}else if(choice ==2) { //student
+			System.out.println("-----------\n" + "1) Login \n" + "2) Sign Up \n" + "3) Exit" + "\n-------------");
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            if (choice == 1) {
+                // Student login section
+                System.out.println("Enter Username: ");
+                String username = sc.nextLine();
+                System.out.println("Enter Password: ");
+                String password = sc.nextLine();
+
+                boolean isValid = checkStudent(username, password);
+
+                if (isValid) {
+                    // Options After Successful login of Student
+                    System.out.println("---------------------\nLogin Successful!\n-----------------");
+                    System.out.println("------------------\n" + "1) See all alumni \n" + "2) See alumni based on preference \n" + "3) See alumni based on username \n" + "4) See all posts\n--------------------");
+                    choice = sc.nextInt();
+
+                    // Perform action based on choice
+                    switch (choice) {
+                        case 1:
+                            //seeAllAlumni();
+                            break;
+                        case 2:
+                            seeAlumniByPreference();
+                            break;
+                        case 3:
+                            //seeAlumniByUsername();
+                            break;
+                        case 4:
+                            //seeAllPosts();
+                            break;
+                        default:
+                            System.out.println("Invalid Choice !");
+                            break;
+                    }
+                } else {
+                    System.out.println("Incorrect Username or Password");
+                }
+            } else if (choice == 2) {
+                // Create new student account
+                Student newStudent = studentInput();
+                System.out.println("Successfully Created Account !");
+            } else if (choice == 3) {
+                System.out.println("Thank you for Connecting !");
+            } else {
+                System.out.println("Invalid Choice !");
+            }
+
 		}else if(choice ==3){
 			System.out.println("Thank you for Connecting !");
 		}else {
