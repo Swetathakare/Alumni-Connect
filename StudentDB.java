c7de262ebe5822056ade40ef8d7599bbe7e4f62a
Sakshi Varhadi <94298487+Sakshi-1602@users.noreply.github.com>
2024-04-21 16:25:55 +0530

    Update StudentDB.java

--- StudentDB.java
+++ StudentDB.java
@@ -8,10 +8,11 @@ public class StudentDB {
     List<Student> studentsList = new ArrayList<>();
 
     StudentDB() {
-        // Initial dummy data
+        studentsList.add(new Student("ABC1234567", "Riddhi Sharma", "9876543210", "riddhi@xyzcollege.in", "Computer Science", 3, 3.8, "Software Development", "Intern at CompanyX"));
+        studentsList.add(new Student("DEF2345678", "Rohan Desh", "9876543211", "rohan@xyzcollege.in", "Electrical Engineering", 2, 3.5, "Data Analysis", "Data Analyst at CompanyY"));
+        studentsList.add(new Student("GHI3456789", "Ali Singh", "9876543212", "ali@xyzcollege.in", "Mechanical Engineering", 4, 3.2, "Product Management", "Product Manager at CompanyZ"));
     }
-
-    void addNewStudent() {
+    /*void addNewStudent() {
         // Add new student logic
     }
 
@@ -21,5 +22,5 @@ public class StudentDB {
 
     public static void main(String[] args) {
         // Main method for testing
-    }
+    }*/
 }
