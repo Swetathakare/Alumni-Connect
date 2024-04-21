eea5ff959ad0478d5d9f6bb4bb9af4d581b8aa8d
Sakshi Varhadi <94298487+Sakshi-1602@users.noreply.github.com>
2024-04-21 15:25:20 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,45 +10,6 @@ public class Main {
 	static boolean check(String alId,String alPass) {
 		return true;
 	}
-	static Alumni alumniInput() {
-
-		System.out.println("-------------------------------------");
-
-		System.out.print("Enter Name             :-");
-		String alName = sc.nextLine();
-
-		System.out.print("Enter Id               :-");
-		String id = sc.nextLine();
-
-		System.out.print("Enter Branch           :-");
-		String branch = sc.nextLine();
-
-		System.out.print("Enter Passing Year     :-");
-		int passingYear = sc.nextInt();
-		sc.nextLine();
-
-		System.out.print("Enter jobProfile       :-");
-		String jobProfile = sc.nextLine();
-
-		System.out.print("Enter Number of fields :-");
-		int numOfDom = sc.nextInt();
-		ArrayList<String> al = new ArrayList<>();
-
-		for (int i = 0; i < numOfDom; i++) {
-			String tmp = sc.nextLine();
-			al.add(tmp);
-		}
-
-		System.out.print("Enter Email Id         :-");
-		String emailId = sc.nextLine();
-
-		System.out.print("Enter Phone No         :-");
-		long phone = sc.nextLong();
-
-		System.out.println("-------------------------------------");
-
-		return new Alumni(alName, branch, passingYear, jobProfile, al, id);
-	}
 
 	static boolean checkStudent(String username, String password) {
         // Dummy implementation for checking student credentials
@@ -76,56 +37,7 @@ public class Main {
                 break;
         }
     }
-
-	static Student studentInput() {
-
-		System.out.println("-------------------------------------");
-
-		System.out.print("Enter Roll Number : ");
-
-		String rollNum = sc.nextLine();
-
-		System.out.print("Enter Name : ");
-
-		String name = sc.nextLine();
-
-		System.out.print("Enter Contact Number : ");
-
-		String contactNo = sc.nextLine();
-
-		System.out.print("Enter Email Id : ");
-
-		String emailId = sc.nextLine();
-
-		System.out.print("Enter Branch : ");
-
-		String branch = sc.nextLine();
-
-		System.out.print("Enter Year of Study : ");
-
-		int yearOfStudy = sc.nextInt();
-
-		sc.nextLine();
-
-		System.out.print("Enter GPA : ");
-
-		double GPA = sc.nextDouble();
-
-		sc.nextLine();
-
-		System.out.print("Enter Area of Interest : ");
-
-		String areaOfInterest = sc.nextLine();
-
-		System.out.print("Enter Work Experience : ");
-
-		String workExperience = sc.nextLine();
-
-		System.out.println("-------------------------------------");
-
-		return new Student(rollNum, name, contactNo, emailId, branch, yearOfStudy, GPA, areaOfInterest, workExperience);
-
-		}
+	
 	static boolean checkStudent(String username, String password) {
         // Dummy implementation for checking student credentials
         return true; // Returning true for demonstration
